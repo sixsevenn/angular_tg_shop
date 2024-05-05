0afe8a05d98695f2e7cabd52845c060e8f5bfa98
ERR <bobibulik@gmail.com>
2024-05-05 16:19:02 +0300

    добавление своих продуктов в  products.service.ts

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -3,160 +3,134 @@ import { Injectable } from "@angular/core";
 const domain = 'https://result.school';
 
 export enum ProductType {
-  Skill = 'skill',
-  Intensive = 'intensive',
-  Course = 'course',
+  Poke = 'poke',
+  Salad = 'salad',
+  Porridge = 'porridge', /* каша */
+  Milkshake = 'milkshake',
+  Smoothie = 'smoothie',
+  Goodies = 'goodies', /* вкусности */
 }
 
 export interface IProduct {
   id: string;
-  text: string;
   title: string;
+  description: string;
   link: string;
   image: string;
-  time: string;
+  structure: string; /* состав */
+  nutritional_value: string; /* пищевая ценность: бжу */
   type: ProductType;
 }
 
-function addDamainToLinkAndImage(product: IProduct) {
-  return {
-    ...product,
-    image: domain + product.image,
-    link: domain + product.link,
-  };
-}
+// function addDamainToLinkAndImage(product: IProduct) {
+//   return {
+//     ...product,
+//     image: domain + product.image,
+//     link: domain + product.link,
+//   };
+// }
 
 const products: IProduct[] = [
   {
-  id: '29',
-  title: 'TypeScript',
-  link: '/products/typescript',
-  image: '/img/icons/products/icon-ts.svg',
-  text: 'Основы, типы, компилятор, классы, generic, утилиты,\
-  декораторы, advanced ...',
-  time: 'С опытом 2 недели',
-  type: ProductType.Skill,
+    id: '1',
+    title: 'Поке с тунцом',
+    description: 'Поке с тунцом - это гавайское блюдо из свежего тунца, риса, овощей и соусов.',
+    link: '/products/poke-tuna',
+    image: '/img/icons/products/icon-poke.svg',
+    structure: 'Тунец, рис, авокадо, огурцы, соусы',
+    nutritional_value: 'Белки: 25г, Жиры: 10г, Углеводы: 35г',
+    type: ProductType.Poke,
   },
   {
-    id: '30',
-    title: 'Git и GitHub',
-    link: '/products/git',
-    image: '',
-    text: 'BLD, история версий, ветвление, удаленный репозиторий\
-    релизы, opensourse ...',
-    time: 'С опытом 2 недели',
-    type: ProductType.Skill,
+    id: '2',
+    title: 'Поке с курицей',
+    description: 'Поке с курицей - это гавайское блюдо из куриного мяса, риса, овощей и соусов.',
+    link: '/products/poke-chicken',
+    image: '/img/icons/products/icon-poke.svg',
+    structure: 'Куриное мясо, рис, авокадо, огурцы, соусы',
+    nutritional_value: 'Белки: 20г, Жиры: 8г, Углеводы: 30г',
+    type: ProductType.Poke,
   },
   {
-    id: '910',
-    title: 'Redux, Redux Toolkit и BobX',
-    link: '/products/state-managers',
-    image: '/img/icons/products/icon-state-managers.svg',
-    text: 'Redux, React Redux, Redux DevTools, Resux Toolkit, RTK Query,\
-    MobX...',
-    time: 'С опытом 2 недели',
-    type: ProductType.Skill,
+    id: '3',
+    title: 'Салат "Цезарь"',
+    description: 'Салат "Цезарь" - классический салат с куриным мясом, сухариками и соусом Цезарь.',
+    link: '/products/caesar-salad',
+    image: '/img/icons/products/icon-salad.svg',
+    structure: 'Куриное мясо, листовой салат, сухарики, соус Цезарь',
+    nutritional_value: 'Белки: 15г, Жиры: 12г, Углеводы: 25г',
+    type: ProductType.Salad,
   },
   {
-    id: '940',
-    title: 'React Advanced',
-    link: '/products/react',
-    image: '/img/icons/products/icon-react.svg',
-    text: 'React JS, Хуки, Формы, React Route v6, Context API,\
-    Оптимизация, Арзитектура, PWA ...',
-    time: 'С опытом 8 недель',
-    type: ProductType.Skill,
+    id: '4',
+    title: 'Салат "Греческий"',
+    description: 'Салат "Греческий" - свежий салат с огурцами, помидорами, сыром фета и маслинами.',
+    link: '/products/greek-salad',
+    image: '/img/icons/products/icon-salad.svg',
+    structure: 'Огурцы, помидоры, сыр фета, маслины, оливковое масло',
+    nutritional_value: 'Белки: 10г, Жиры: 15г, Углеводы: 20г',
+    type: ProductType.Salad,
   },
   {
-    id: '920',
-    title: 'Первая ступень профессии frontend-разработчика',
-    link: '/products/first-stage',
-    image: '',
-    text: 'JavaScript, Debug, DOM...',
-    time: 'С нуля 3 месяца',
-    type: ProductType.Skill,
+    id: '5',
+    title: 'Овсяная каша с фруктами',
+    description: 'Овсяная каша с фруктами - полезный завтрак из овсянки с свежими фруктами.',
+    link: '/products/oatmeal-fruit',
+    image: '/img/icons/products/icon-porridge.svg',
+    structure: 'Овсянка, молоко, фрукты',
+    nutritional_value: 'Белки: 7г, Жиры: 3г, Углеводы: 40г',
+    type: ProductType.Porridge,
   },
   {
-    id: '930',
-    title: 'Первая ступень профессии frontend-разработчика',
-    link: '/products/second-stage',
-    image: '',
-    text: 'JavaScript, Debug, DOM...',
-    time: 'С нуля 3 месяца',
-    type: ProductType.Skill,
+    id: '6',
+    title: 'Молочный коктейль с бананом',
+    description: 'Молочный коктейль с бананом - напиток на молоке с добавлением свежего банана.',
+    link: '/products/milkshake-banana',
+    image: '/img/icons/products/icon-milkshake.svg',
+    structure: 'Молоко, банан, мед, ваниль',
+    nutritional_value: 'Белки: 5г, Жиры: 3г, Углеводы: 30г',
+    type: ProductType.Milkshake,
   },
   {
-    id: '24',
-    title: 'Первая ступень профессии frontend-разработчика',
-    link: '/products/base-programming',
-    image: '',
-    text: 'JavaScript, Debug, DOM...',
-    time: 'С нуля 3 месяца',
-    type: ProductType.Intensive,
+    id: '7',
+    title: 'Смузи "Зеленый"',
+    description: 'Смузи "Зеленый" - напиток из свежих зеленых овощей и фруктов.',
+    link: '/products/smoothie-green',
+    image: '/img/icons/products/icon-smoothie.svg',
+    structure: 'Шпинат, банан, яблоко, апельсин',
+    nutritional_value: 'Белки: 5г, Жиры: 2г, Углеводы: 25г',
+    type: ProductType.Smoothie,
   },
   {
-    id: '32',
-    title: 'Первая ступень профессии frontend-разработчика',
-    link: '/products/demo-week',
-    image: '',
-    text: 'JavaScript, Debug, DOM...',
-    time: 'С нуля 3 месяца',
-    type: ProductType.Intensive,
+    id: '8',
+    title: 'Десертные вкусности',
+    description: 'Десертные вкусности - сладкие угощения из фруктов и ягод.',
+    link: '/products/goodies-dessert',
+    image: '/img/icons/products/icon-goodies.svg',
+    structure: 'Фрукты, ягоды, сироп, шоколад',
+    nutritional_value: 'Белки: 2г, Жиры: 5г, Углеводы: 20г',
+    type: ProductType.Goodies,
   },
   {
-    id: '33',
-    title: 'Первая ступень профессии frontend-разработчика',
-    link: '/products/advanced-js',
-    image: '',
-    text: 'JavaScript, Debug, DOM...',
-    time: 'С нуля 3 месяца',
-    type: ProductType.Intensive,
+    id: '9',
+    title: 'Сэндвич с индейкой',
+    description: 'Сэндвич с индейкой - перекус из тонкой лавашной основы с нежным мясом индейки.',
+    link: '/products/sandwich-turkey',
+    image: '/img/icons/products/icon-sandwich.svg',
+    structure: 'Лаваш, индейка, овощи, соус',
+    nutritional_value: 'Белки: 15г, Жиры: 8г, Углеводы: 30г',
+    type: ProductType.Poke,
   },
   {
-    id: '28',
-    title: 'Курс "основы JavaScript" и 50 заданий',
-    link: '/products/javascript',
-    image: '',
-    text: 'JavaScript, Debug, DOM...',
-    time: 'С нуля 3 месяца',
-    type: ProductType.Course,
-  },
-  {
-    id: '23',
-    title: 'Курс "основы JavaScript" и 50 заданий',
-    link: '/products/html-css',
-    image: '',
-    text: 'JavaScript, Debug, DOM...',
-    time: 'С нуля 3 месяца',
-    type: ProductType.Course,
-  },
-  {
-    id: '26',
-    title: 'Курс "основы JavaScript" и 50 заданий',
-    link: '/products/html-css',
-    image: '',
-    text: 'JavaScript, Debug, DOM...',
-    time: 'С нуля 3 месяца',
-    type: ProductType.Course,
-  },
-  {
-    id: '27',
-    title: 'Курс "основы JavaScript" и 50 заданий',
-    link: '/products/html-css',
-    image: '',
-    text: 'JavaScript, Debug, DOM...',
-    time: 'С нуля 3 месяца',
-    type: ProductType.Course,
-  },
-  {
-    id: '930',
-    title: 'Воркщоп "перспективы в IT"',
-    link: '/products/perspectives',
-    image: '',
-    text: 'JavaScript, Debug, DOM...',
-    time: 'С нуля 2 дня',
-    type: ProductType.Course,
-  },
+    id: '10',
+    title: 'Мясной пирог',
+    description: 'Мясной пирог - традиционное блюдо из слоеного теста с начинкой из мяса и лука.',
+    link: '/products/meat-pie',
+    image: '/img/icons/products/icon-sandwich.svg',
+    structure: 'Говядина, мука, вода',
+    nutritional_value: 'Белки: 15г, Жиры: 8г, Углеводы: 30г',
+    type: ProductType.Goodies,
+  }
 ];
 
 @Injectable({
