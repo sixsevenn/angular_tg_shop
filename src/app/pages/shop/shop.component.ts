6ecfe92999ba211ace8ea6d17b9d590a0c2e4d4e
ERR <bobibulik@gmail.com>
2024-05-08 13:41:43 +0300

    добавление функции перехождения на страничку товара

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -3,6 +3,7 @@ import { TelegramService } from '../../services/telegram.service';
 import { ProductsSevice, IProduct } from '../../services/products.service';
 import { ProductListComponent } from '../../components/product-list/product-list.component';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 
 
@@ -10,20 +11,22 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [ProductListComponent,CommonModule],
+  imports: [ProductListComponent,CommonModule, RouterLink],
   template: `
     <ng-container *ngFor="let productChunk of chunk(productsService.products, 2)">
       <div class="wrapper">
         <div class="card" *ngFor="let product of productChunk">
-          <div class="img_div">
-            <img class="card-img" src="assets/image/yt_profil1.png">
-          </div>
-          <div class="card-title">{{ product.title }}<br>{{ product.size }}</div>
-          <div class="btn-keeper">
-            <a class="btn" [href]="product.link">Заказать</a>
-          </div>
-          <div class="price">{{ product.price }} ₽</div> 
-          <div class="weight">{{ product.weight }} г</div>
+          <li class="product-item" [routerLink]="'/product/' + product.id">
+            <div class="img_div">
+              <img class="card-img" src="assets/image/yt_profil1.png">
+            </div>
+            <div class="card-title">{{ product.title }}<br>{{ product.size }}</div>
+            <div class="btn-keeper">
+              <a class="btn" [href]="product.link">Заказать</a>
+            </div>
+            <div class="price">{{ product.price }} ₽</div> 
+            <div class="weight">{{ product.weight }} г</div>
+          </li>
         </div>
       </div>
     </ng-container>
