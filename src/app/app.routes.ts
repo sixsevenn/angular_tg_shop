678988e6960c466535051fadcaad04f07a580e95
sixSeven <vladiabud2@gmail.com>
2024-06-15 15:16:00 +0300

    добавление странички shippingPayment на ней пользователь будет выбирать адрес доставки и способ оплаты

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { FeedbackComponent } from './pages/feedback/feedback.component';
 import { ProductComponent } from './pages/product/product.component';
 // import { AdminComponent } from './pages/admin/admin.component';
 import { BasketComponent } from './pages/basket/basket.component';
+import { ShippingPaymentComponent } from './pages/shippingPayment/shippingPayment.component';
 
 export const routes: Routes = [
     { path: '', component: ShopComponent, pathMatch: 'full'},
@@ -11,4 +12,5 @@ export const routes: Routes = [
     {path: 'product/:id', component: ProductComponent},
     // {path: 'admin', component: AdminComponent},
     {path: 'basket', component: BasketComponent},
+    {path: 'shippingPayment', component: ShippingPaymentComponent},
 ];
